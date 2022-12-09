018d76944c52cf7cee7add036d1c53110fae36b2
ppeip <wawaro@163.com>
2022-12-09 19:45:02 +0800

    first commit

--- internal/repository/model.go
+++ internal/repository/model.go
@@ -1,22 +1,48 @@
 package repository
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
 type AgPrice struct {
-	No            int64   `json:"no" gorm:"primaryKey;autoIncrement " `
-	Variety       string  `json:"variety"`   /*品种:"Ag(T+D)"*/
-	Latestpri     float32 `json:"latestpri"` /*最新价:"6585.00"*/
-	Openpri       float32 `json:"openpri"`   /*开盘价:"6712.00"*/
-	Maxpri        float32 `json:"maxpri"`    /*最高价:"6721.00"*/
-	Minpri        float32 `json:"minpri"`    /*最低价:"6581.00"*/
-	Limit         string  `json:"limit"`     /*涨跌幅:"-1.98%"*/
-	Yespri        float32 `json:"yespri"`    /*昨收价:"6718.00"*/
-	Totalvol      float64 `json:"totalvol"`  /*总成交量:"1564524.0000"*/
-	CreatedAtTime string  `json:"time"`      /*更新时间:"2012-12-19 15:29:59"*/
+	Id            int64   `json:"id" gorm:"primaryKey;autoIncrement " `
+	Variety       string  `json:"variety"` /*品种:"Ag(T+D)"*/
+	Varietycnname string  `json:"cnname"`
+	Latestpri     float32 `json:"latestpri"`                 /*最新价:"6585.00"*/
+	Openpri       float32 `json:"openpri"`                   /*开盘价:"6712.00"*/
+	Maxpri        float32 `json:"maxpri"`                    /*最高价:"6721.00"*/
+	Minpri        float32 `json:"minpri"`                    /*最低价:"6581.00"*/
+	Limit         string  `json:"limit"`                     /*涨跌幅:"-1.98%"*/
+	Yespri        float32 `json:"yespri"`                    /*昨收价:"6718.00"*/
+	Totalvol      float64 `json:"totalvol"`                  /*总成交量:"1564524.0000"*/
+	CreatedAtTime string  `json:"time" gorm:"type:datetime"` /*更新时间:"2012-12-19 15:29:59"*/
+}
+
+type MetalType struct {
+	Id            int64   `json:"id"`
+	Variety       string  `json:"variety" gorm:"primaryKey"`
+	Totalvol      float64 `json:"totalvol"`
+	Varietycnname string  `json:"cnname"`
+}
+
+func (agPrice *AgPrice) AfterCreate(db *gorm.DB) (err error) {
+	var metalType MetalType
+	fmt.Println(agPrice.Totalvol)
+	db.Model(&metalType).Where("variety = ?", agPrice.Variety).Update("totalvol", gorm.Expr("totalvol + ?", agPrice.Totalvol))
+	fmt.Println("修改成功")
+	return nil
+}
+
+func (agPrice *AgPrice) BeforeDelete(db *gorm.DB) (err error) {
+	var metalType MetalType
+	fmt.Println(agPrice.Totalvol)
+	db.Model(&metalType).Where("variety = ?", agPrice.Variety).Update("totalvol", gorm.Expr("totalvol - ?", agPrice.Totalvol))
+	fmt.Println("修改成功")
+	return nil
 }
 
 func AutoMigrate(db *gorm.DB) error {
-	return db.AutoMigrate(&AgPrice{})
+	return db.AutoMigrate(&AgPrice{}, &MetalType{})
 }
--- internal/router/initrouter.go
+++ internal/router/initrouter.go
@@ -12,34 +12,110 @@ import (
 
 func SetupRouter(db *gorm.DB) *gin.Engine {
 	router := gin.Default()
-	router.GET("/pricerecord/:no", func(context *gin.Context) {
-		no := context.Param("no")
+	router.Use(CORSMiddleware())
+	//根据id获取一条数据
+	router.GET("api/records/:id", func(context *gin.Context) {
+		id := context.Param("id")
 		var agPrice repository.AgPrice
-		err := db.First(&agPrice, "no = ?", no).Error
+		err := db.First(&agPrice, "id = ?", id).Error
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			context.String(http.StatusNotFound, "pricerecord not found")
 			return
 		}
 		context.JSON(http.StatusOK, agPrice)
 	})
-
-	router.POST("/pricerecord", func(context *gin.Context) {
+	//获取所有的数据
+	router.GET("api/allrecords", func(context *gin.Context) {
+		var agPrice []repository.AgPrice
+		err := db.Find(&agPrice).Error
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			context.String(http.StatusNotFound, "No pricerecord")
+			return
+		}
+		context.JSON(http.StatusOK, agPrice)
+	})
+	//获取多少种类金属以及是什么
+	router.GET("api/records/varieties", func(context *gin.Context) {
+		var metalType []repository.MetalType
+		type Res struct {
+			Variety       string `json:"variety" `
+			Varietycnname string `json:"cnname"`
+		}
+		var res []Res
+		result := db.Model(&metalType).Find(&res)
+		err := result.Error
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			context.String(http.StatusNotFound, "pricerecord not found")
+			return
+		}
+		context.JSON(http.StatusOK, gin.H{
+			"variety_num": result.RowsAffected,
+			"variety":     res,
+		})
+	})
+	//根据金属种类获取所有数据
+	router.GET("api/records/variety/:variety", func(context *gin.Context) {
+		variety := context.Param("variety")
+		var agPrice []repository.AgPrice
+		err := db.Find(&agPrice, "variety = ?", variety).Error
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			context.String(http.StatusNotFound, "pricerecord not found")
+			return
+		}
+		context.JSON(http.StatusOK, agPrice)
+	})
+	//返回七天内平均交易量
+	router.GET("/api/aver/:variety", func(context *gin.Context) {
+		variety := context.Param("variety")
+		var agPrice []repository.AgPrice
+		type Avg_Totalvols struct {
+			Avg_totalvol float64
+		}
+		var avg_totalvols Avg_Totalvols
+		//明天用gorm重构一下
+		//err := db.Raw("SELECT avg(totalvol) AS avg_totalvol FROM (SELECT variety,created_at_time,totalvol  FROM ag_prices   WHERE `variety` = ? ORDER BY created_at_time DESC LIMIT 7) TEMP ", variety).Scan(&avg_totalvols).Error
+		err := db.Table("(?) as temp ", db.Select("variety", "created_at_time", "totalvol").Find(&agPrice).Where("variety = ?", variety).
+			Order("created_at_time DESC").Limit(7)).
+			Select("avg(totalvol) AS avg_totalvol").
+			Scan(&avg_totalvols).Error
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			context.String(http.StatusNotFound, "pricerecord not found")
+			return
+		}
+		context.JSON(http.StatusOK, gin.H{
+			"vol": avg_totalvols.Avg_totalvol,
+		})
+	})
+	//返回总交易量
+	router.GET("api/total/:variety", func(context *gin.Context) {
+		variety := context.Param("variety")
+		var metalType repository.MetalType
+		err := db.First(&metalType, "variety = ?", variety).Error
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			context.String(http.StatusNotFound, "pricerecord not found")
+			return
+		}
+		context.JSON(http.StatusOK, gin.H{
+			"vol": metalType.Totalvol,
+		})
+	})
+	//向数据库中添加新的数据
+	router.POST("api/records", func(context *gin.Context) {
 		var agPrice repository.AgPrice
 		err := context.BindJSON(&agPrice)
 		if err != nil {
-			println("json格式不正确")
-			context.JSON(406, gin.H{"message": "Invalid signin json", "json": agPrice})
+			context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid signin json", "json": agPrice})
 			context.Abort()
 			return
 		}
 		db.Create(&agPrice)
 		context.Status(http.StatusCreated)
 	})
-
-	router.PUT("/pricerecord/:no", func(context *gin.Context) {
+	//根据id修改现有数据
+	router.PUT("api/records/:id", func(context *gin.Context) {
 		var agPrice repository.AgPrice
-		no := context.Param("no")
-		err := db.First(&agPrice, "no =?", no).Error
+		id := context.Param("id")
+		err := db.First(&agPrice, "id =?", id).Error
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			context.String(http.StatusNotFound, "pricerecord not found")
 			return
@@ -49,13 +125,18 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 			return
 		}
 		db.Save(&agPrice)
-		context.Status(http.StatusNoContent)
+		context.Status(http.StatusOK)
 	})
-
-	router.DELETE("/pricerecord/:no", func(context *gin.Context) {
-		no := context.Param("no")
+	//根据id删除现有的某条数据
+	router.DELETE("api/records/:id", func(context *gin.Context) {
+		id := context.Param("id")
 		var agPrice repository.AgPrice
-		err := db.Where("no = ? ", no).Delete(&agPrice).Error
+		err := db.First(&agPrice, "id =?", id).Error
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			context.String(http.StatusNotFound, "pricerecord not found")
+			return
+		}
+		err = db.Where("id = ? ", id).Delete(&agPrice).Error
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			context.String(http.StatusNotFound, "pricerecord not found")
 			return
@@ -63,4 +144,20 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 	})
 
 	return router
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT,DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
